2cd4702454006aea9e13904265ff8ccaed8e3261
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-08-31 12:50:12 +0530

    devops practice

--- wordoccurences.sh
+++ wordoccurences.sh
@@ -11,6 +11,7 @@ USAGE (){
 if [ $# -lt 2 ]
 then
     USAGE
+    exit 1
 fi
 
 if [ ! -f $file ]
@@ -20,4 +21,12 @@ then
 else
     echo "$file exists"
 fi
+
+awk 'BEGIN{ FS="[ \t\n]+" } { for (i=1; i<=NF; i++) count[$i]++ } END{ for (word in count) print word, count[word] }' "$2" |
+# Sort output by count in descending order, then by word alphabetically
+sort -k2,2nr -k1,1 |
+# Display top 5 most frequent words
+head -n 5 |
+# Format output
+awk '{ printf("%-15s : %d\n", $1, $2) }'
 
