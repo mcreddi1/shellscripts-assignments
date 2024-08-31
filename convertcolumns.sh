fd6465e2674aeae16558390c9d5eae637259bda6
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-08-31 18:23:31 +0530

    devops practice

--- convertcolumns.sh
+++ convertcolumns.sh
@@ -41,18 +41,17 @@ END {
 # columns back to rows using awk
 echo ""
 echo "Converted back to rows:"
-#awk '{
-#    for (i=1; i<=NF; i++) {
-#        if (NR == 1) {
-#            row[i] = $i
-#        } else {
-#            row[i] = row[i] " " $i
-#        }
-#    }
-#}
-#END {
-#    for (i=1; i<=NF; i++) {
-#        print row[i]
-#    }
-#}' $file
-#
+awk '{
+    for (i=1; i<=NF; i++) {
+        if (NR == 1) {
+            row[i] = $i
+        } else {
+            row[i] = row[i] " " $i
+        }
+    }
+}
+END {
+    for (i=1; i<=NF; i++) {
+        print row[i]
+    }
+}' $file
