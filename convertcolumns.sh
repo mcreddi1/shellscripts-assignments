9fd4b3c6a5f71c95eed3de76b222a7e64f50d8e6
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-08-31 18:20:42 +0530

    devops practice

--- convertcolumns.sh
+++ convertcolumns.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Sample input file (rows.txt)
+# Sample input file (file.txt)
 # Replace this with your actual data file or provide input directly in the script
 file=$1
 
@@ -20,38 +20,39 @@ echo "Original data (rows):"
 cat $file
 echo ""
 
-# Convert rows to columns using awk
-echo "Converted to columns:"
-awk '{
-    for (i=1; i<=NF; i++) {
-        if (NR == 1) {
-            col[i] = $i
-        } else {
-            col[i] = col[i] " " $i
-        }
-    }
-}
-END {
-    for (i=1; i<=NF; i++) {
-        print col[i]
-    }
-}' $Files
-
-
-convert columns back to rows using awk
-echo ""
-echo "Converted back to rows:"
-awk '{
-    for (i=1; i<=NF; i++) {
-        if (NR == 1) {
-            row[i] = $i
-        } else {
-            row[i] = row[i] " " $i
-        }
-    }
-}
-END {
-    for (i=1; i<=NF; i++) {
-        print row[i]
-    }
-}' $file
+## Convert rows to columns using awk
+#echo "Converted to columns:"
+#awk '{
+#    for (i=1; i<=NF; i++) {
+#        if (NR == 1) {
+#            col[i] = $i
+#        } else {
+#            col[i] = col[i] " " $i
+#        }
+#    }
+#}
+#END {
+#    for (i=1; i<=NF; i++) {
+#        print col[i]
+#    }
+#}' $file
+#
+#
+#convert columns back to rows using awk
+#echo ""
+#echo "Converted back to rows:"
+#awk '{
+#    for (i=1; i<=NF; i++) {
+#        if (NR == 1) {
+#            row[i] = $i
+#        } else {
+#            row[i] = row[i] " " $i
+#        }
+#    }
+#}
+#END {
+#    for (i=1; i<=NF; i++) {
+#        print row[i]
+#    }
+#}' $file
+#
