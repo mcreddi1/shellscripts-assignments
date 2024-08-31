6f093cbadb2c5b1598671e19c0e3444755802d81
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-08-31 13:51:26 +0530

    devops practice

--- convertcolumns.sh
+++ convertcolumns.sh
@@ -4,6 +4,17 @@
 # Replace this with your actual data file or provide input directly in the script
 file=$1
 
+USAGE(){
+
+    echo "USAGE: sh <scriptname> <filename>"
+}
+
+if [ $# -lt 1 ]
+then
+    echo "usage: $USAGE"
+    exit 1
+fi
+
 echo "Original data (rows):"
 cat $file
 echo ""
@@ -25,20 +36,22 @@ END {
     }
 }' $file
 
-# Convert columns back to rows using awk
-echo ""
-echo "Converted back to rows:"
-awk '{
-    for (i=1; i<=NF; i++) {
-        if (NR == 1) {
-            row[i] = $i
-        } else {
-            row[i] = row[i] " " $i
-        }
-    }
-}
-END {
-    for (i=1; i<=NF; i++) {
-        print row[i]
-    }
-}' $file
+
+## Convert columns back to rows using awk
+#echo ""
+#echo "Converted back to rows:"
+#awk '{
+#    for (i=1; i<=NF; i++) {
+#        if (NR == 1) {
+#            row[i] = $i
+#        } else {
+#            row[i] = row[i] " " $i
+#        }
+#    }
+#}
+#END {
+#    for (i=1; i<=NF; i++) {
+#        print row[i]
+#    }
+#}' $file
+#
