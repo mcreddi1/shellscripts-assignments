6f254b78ada0b57e5bec54d318ea04311ebdb81c
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-08-31 18:22:03 +0530

    devops practice

--- convertcolumns.sh
+++ convertcolumns.sh
@@ -20,27 +20,27 @@ echo "Original data (rows):"
 cat $file
 echo ""
 
-## Convert rows to columns using awk
-#echo "Converted to columns:"
-#awk '{
-#    for (i=1; i<=NF; i++) {
-#        if (NR == 1) {
-#            col[i] = $i
-#        } else {
-#            col[i] = col[i] " " $i
-#        }
-#    }
-#}
-#END {
-#    for (i=1; i<=NF; i++) {
-#        print col[i]
-#    }
-#}' $file
-#
-#
-#convert columns back to rows using awk
-#echo ""
-#echo "Converted back to rows:"
+# Convert rows to columns using awk
+echo "Converted to columns:"
+awk '{
+    for (i=1; i<=NF; i++) {
+        if (NR == 1) {
+            col[i] = $i
+        } else {
+            col[i] = col[i] " " $i
+        }
+    }
+}
+END {
+    for (i=1; i<=NF; i++) {
+        print col[i]
+    }
+}' $file
+
+
+convert columns back to rows using awk
+echo ""
+echo "Converted back to rows:"
 #awk '{
 #    for (i=1; i<=NF; i++) {
 #        if (NR == 1) {
