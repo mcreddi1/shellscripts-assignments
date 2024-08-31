afd4072f0660b21987bda555f89148c8be2830b1
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-08-31 13:25:41 +0530

    devops practice

--- wordoccurences.sh
+++ wordoccurences.sh
@@ -22,9 +22,15 @@ else
     echo "$1 exists"
 fi
 
-while IFS= read -r line
-do
-    echo "searching lines: $file"
-count=$(cat $file | grep $searchword | wc -w)
-echo "$searchword=$count"
-done <<<$file
+#while IFS= read -r line
+#do
+#    echo "searching lines: $file"
+#count=$(cat $file | grep $searchword | wc -w)
+#echo "$searchword=$count"
+#done <<<$file
+
+
+wk '{ for (i=1; i<=NF; i++) words[$i]++ } END { for (word in words) print word, words[word] }' "$searchword" |
+sort -k2,2nr -k1,1 |
+# Filter out words with count less than or equal to 1
+awk '$2 > 1 { print }'
