61be21c87bc8af3a2992506c76afce8b512ee9ae
mcreddi1 <morachinnapareddy1996@gmail.com>
2024-09-16 12:55:49 +0530

    terraform

--- githubrepoaccess.sh
+++ githubrepoaccess.sh
@@ -5,7 +5,7 @@ USERNAME="mcreddi1"
 REPO="devopspractice"
 
 # Replace with your GitHub personal access token
-TOKEN="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"
+TOKEN= $1 #please use your github PAT(personal access token)
 
 # GitHub API endpoint to list repository collaborators
 URL="https://api.github.com/repos/${USERNAME}/${REPO}/collaborators"
